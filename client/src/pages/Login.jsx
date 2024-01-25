7db74aec5cfae76631175ee048d3a18d23851c60
jaylenberrien <jaylenberrien.jb@gmail.com>
2024-01-24 20:16:29 -0500

    login page basic structure done

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -7,16 +7,18 @@ export default function Login () {
         <h1 className='text-white text-8xl m-auto text-right'>Ready for the trip of a lifetime?</h1>
       </div>
       <div className='flex h-screen w-1/2'>
-          <div className='m-auto bg-slate-300 p-10 flex'> 
-              <h2></h2>
+          <div className='m-auto bg-slate-300 p-10 flex-block text-right'> 
+              <h2 className='text-center'>Login</h2>
               <br /><br />
-              <form action=''>
+              <form action='' className='text-center'>
                   Username: <input type='text' className=''/>
                   <br /><br />
                   Password: <input type='password' className=''/>
                   <br /><br />
-                  <input type='submit' value='Submit' className='text-white bg-gray-500 ml-auto mr-auto'/>
+                  <input type='submit' value='Submit' className='text-white bg-gray-500'/>
               </form>
+              <br />
+              <a href='' className='underline decoration-solid text-sm'>Click here to register</a>
           </div>
       </div>
     </div>
