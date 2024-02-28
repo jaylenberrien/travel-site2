046a7393fadcb75d5df8105d21e437b3ef1bca13
jaylenberrien <jaylenberrien.jb@gmail.com>
2024-02-28 04:43:41 -0500

    forgot to commit before

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,26 @@
+const { signupUser, loginUser } = require('../controllers/authController')
+//try to remove the code above this line after we set up the routes i dont think we need this
+const express = require('express')
+const router = express.Router()
+
+
+//login route
+router.post('/login', ()=>{})
+
+//signup route
+router.post('/signup', ()=>{})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router
