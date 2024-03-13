06801ef6f839488afcd298254f42b882b889440a
jaylenberrien <jaylenberrien.jb@gmail.com>
2024-03-13 00:29:03 -0400

    updated nav bar to have basic dropdown menu  that has a link to account page as well as a functional log out button

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,23 +1,34 @@
 import {Link} from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
  
 export default function Nav(){
   return(
     <div className="flex bg-black text-white">
-        <div className="justify-start p-4">Travel Site</div>
+      <div className="justify-start p-4">Travel Site</div>
 
-        <div className="flex mx-auto">
-            <Link to="/">
-                <div className="p-4">Discover</div>
-            </Link>
-            <Link to="/about">
-                <div className="p-4">About</div>
-            </Link>
-        </div>
-        <div>
-            <Link to="/account">
-                <div className="p-5 bg-gray-600 rounded-full m-2" ></div>
-            </Link>
+      <div className="flex mx-auto">
+          <Link to="/">
+              <div className="p-4">Discover</div>
+          </Link>
+          <Link to="/about">
+              <div className="p-4">About</div>
+          </Link>
+      </div>
+      <div class='drop-container' >  
+        <div className="p-5 bg-gray-600 rounded-full m-2" ></div>
+        <div class='drop-menu'>  
+          <div class='drop-container'>
+            <Link to='/account'>
+              <div>Account</div>
+            </Link>  
           </div>
+          <div class='drop-container'>
+            <div onClick={useLogout()}>Logout</div>
+          </div>
+        </div>
+      </div>
+
+      
     </div>
 
   )
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router} from 'react-router-dom'
 import App from './App.jsx'
 import './App.css'
+import './Nav.css'
 import { AuthContextProvider } from './context/AuthContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
