38a63c3c0a9bfa596dfa68d80aad5cd48c6eaa62
jaylenberrien <jaylenberrien.jb@gmail.com>
2024-03-19 02:14:10 -0400

    added frame of popup component

--- client/src/components/TripEditor.jsx
+++ client/src/components/TripEditor.jsx
@@ -2,7 +2,7 @@ export function TripEditor (){
 
 
     return(
-        <div className="p-15 bg-blue-300 absolute center">
+        <div className="p-15 bg-blue-300 absolute center left-80 top-72 right-80 bottom-72 text-center align-middle">
             test
         </div>
     )
