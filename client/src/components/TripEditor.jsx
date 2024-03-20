a1c4da9792d0d86bba3be6693673c80dbc04b9c3
jaylenberrien <jaylenberrien.jb@gmail.com>
2024-03-20 08:41:12 -0400

    added fixed property to component

--- client/src/components/TripEditor.jsx
+++ client/src/components/TripEditor.jsx
@@ -2,12 +2,13 @@ export function TripEditor (){
 
 
     return(
-        <div className="flex flex-col p-15 bg-blue-300 absolute center left-80 top-72 right-80 bottom-72 text-center align-middle">
+        <div className="flex flex-col p-15 bg-blue-300 fixed center left-80 top-72 right-80 bottom-72 text-center align-middle">
             <div className="mx-auto mt-2">test</div>
             <div className="bg-purple-400 my-auto">
                 <div className=" bg-red-400 my-4 h-24 w-60 mx-auto flex">
                     <div className="mx-auto my-auto">test2</div>
                 </div>
+                <div>or</div>
                 <div className=" bg-red-400 mt-4 h-24 w-60 mx-auto flex">
                     <div className="mx-auto my-auto">test3</div>
                 </div>
