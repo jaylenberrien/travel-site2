5a615f7ed5f7afef077b487e39a947a1aa5cc490
jaylenberrien <jaylenberrien.jb@gmail.com>
2024-03-24 10:34:17 -0400

    the old trip component can now pop and disappear

--- client/src/components/OldTrip.jsx
+++ client/src/components/OldTrip.jsx
@@ -1,17 +1,17 @@
 import { useState } from "react"
 import { OldTripOpt } from "./OldTripOpt"
 
-export function OldTrip ({ showTrips, setShowTrips, handleHideTrips }){
+export function OldTrip ({ showTrips, setShowTrips }){
 
-    // const handleRemoveTrips = () =>{
-    //     setShowTrips(false)
-    // }
-
+    const remove = () =>{
+        setShowTrips(false)
+    }
+    
     return(
         <div className="flex flex-col p-15 bg-pink-300 fixed center left-80 top-72 right-80 bottom-72 text-center align-middle shadow-2xl z-50">
             <div>Select Trip</div>
             <OldTripOpt />
-            <div onClick={handleHideTrips} className="bg-red-400 w-12 mx-auto">Cancel</div>
+            <div onClick={remove} className="bg-red-400 w-12 mx-auto">Cancel</div>
         </div>
     )
 }
--- client/src/components/TripEditor.jsx
+++ client/src/components/TripEditor.jsx
@@ -1,36 +1,37 @@
-import { OldTrip } from "./OldTrip"
-import { useState } from "react"
+// 
 
-export function TripEditor ({showSelector, setShowComponent}){
+import { OldTrip } from "./OldTrip";
+import { useState } from "react";
 
-    const [showTrips, setShowTrips] = useState(false)
+export function TripEditor({ showSelector, setShowComponent }) {
+  const [showTrips, setShowTrips] = useState(false);
 
-    const handleCancelClick = () =>{
-        setShowComponent(false)
-    }
+  const handleCancelClick = () => {
+    setShowComponent(false);
+  };
 
-    const handleShowTrips = () =>{
-        setShowTrips(true)
-    }
+  const handleShowTrips = () => {
+    setShowTrips(true);
+  };
 
-    const handleHideTrips = () =>{
-        setShowTrips(false)
-    }
-
-
-    return(
-        <div className="flex flex-col p-15 bg-blue-300 fixed center left-80 top-72 right-80 bottom-72 text-center align-middle shadow-2xl z-40">
-            <div className="bg-purple-400 my-auto">
-                <div onClick={handleShowTrips} className=" bg-red-400 my-4 h-24 w-60 mx-auto flex">
-                    {showTrips && <OldTrip showTrips={showTrips} setShowTrips={setShowTrips} handleHideTrips={handleHideTrips} />}
-                    <div className="mx-auto my-auto">Add to trip</div>
-                </div>
-                <div>or</div>
-                <div className=" bg-red-400 mt-4 h-24 w-60 mx-auto flex">
-                    <div className="mx-auto my-auto">Create new trip</div>
-                </div>
-            </div>
-            <div onClick={handleCancelClick} className="mb-2 bg-red-400 w-12 mx-auto">Cancel</div>
+  return (
+    <div className="flex flex-col p-15 bg-blue-300 fixed center left-80 top-72 right-80 bottom-72 text-center align-middle shadow-2xl z-40">
+      <div className="bg-purple-400 my-auto">
+        <div
+          onClick={handleShowTrips}
+          className=" bg-red-400 my-4 h-24 w-60 mx-auto flex"
+        >
+          <div className="mx-auto my-auto">Add to trip</div>
         </div>
-    )
+        {showTrips && <OldTrip showTrips={showTrips} setShowTrips={setShowTrips} />}
+        <div>or</div>
+        <div className=" bg-red-400 mt-4 h-24 w-60 mx-auto flex">
+          <div className="mx-auto my-auto">Create new trip</div>
+        </div>
+      </div>
+      <div onClick={handleCancelClick} className="mb-2 bg-red-400 w-12 mx-auto">
+        Cancel
+      </div>
+    </div>
+  );
 }
