ca799a01a9a47a8691ef0f1779c9637c98eac839
jaylenberrien <jaylenberrien.jb@gmail.com>
2024-04-15 06:43:04 -0400

    updated login screen styling

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -31,25 +31,28 @@ export default function Login () {
   }
 
   return (
-    <div className='inline-block' class='login-container'>
-      <div class='login-background' className='bg-black h-screen w-1/2 flex my-auto ml-5 mt-5 mb-5'>
-        <h1 className='text-white text-8xl m-auto text-right'>Ready for the trip of a lifetime?</h1>
-      </div>
-      <div className='flex h-screen w-1/2'>
-          <div className='m-auto bg-slate-300 p-10 flex-block text-right'> 
-              <h2 className='text-center'>Login</h2>
-              <br /><br />
-              <form onSubmit={handleSubmit} className='text-center'>
-                Username: <input type='text' value={username} onChange={(e) => handleChange(e.target.value)} className=''/>
-                <br /><br />
-                Password: <input type='password' value={password} onChange={handlePasswordChange} className=''/>
-                <br /><br />
-                <input disabled={isLoading} type='submit' value='Submit' className='text-white bg-gray-500'/>
-              </form>
-              <br />
-              {error && <div className='error'>{error}</div>}
-              <a href='' className='underline decoration-solid text-sm'>Click here to register</a>
-          </div>
+    <div className='h-screen w-screen flex' class='login-container'>
+      <div className='bg-green-300 h-3/4 w-3/4 m-auto flex shadow-2xl' >
+        <div class='login-background' className='bg-black h-5/6 w-5/6 m-10 flex my-auto '>
+          {/* <h1 className='text-white text-8xl m-auto text-right'>Ready for the trip of a lifetime?</h1> */}
+          <img src='https://static.toiimg.com/photo/107526341.cms' className='w-full'/>
+        </div>
+        <div className='p-10 text-left w-60 m-10 h-5/6 my-auto'> 
+          <h2 className='text-left text-4xl font-medium '>Login</h2>
+          <br /><br />
+          <form onSubmit={handleSubmit} className='text-left'>
+            Username: <input type='text' placeholder='Enter username' className='placeholder-gray-500 placeholder-opacity-50 ' value={username} onChange={(e) => handleChange(e.target.value)} />
+            <br /><br />
+            Password: <input type='password' placeholder='Enter password' className='placeholder-gray-500 placeholder-opacity-50' value={password} onChange={handlePasswordChange} />
+            <br /><br />
+            <input disabled={isLoading} type='submit' value='Submit' className='text-white bg-gray-500 w-44 mb-3'/>
+          </form>
+          {/* <br /> */}
+          {error && <div className='text-sm text-red-700 my-2'>{error}</div>}
+          <p className='text-sm'>Dont have an account?</p>
+          <button className=' text-white bg-gray-500 w-44'>Create Account</button>
+        </div>
+  
       </div>
     </div>
   )
